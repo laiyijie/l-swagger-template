20ef73793a0c041d3d9fecfdb963b736ea6e7aee
laiyijie <449132062@qq.com>
2017-09-28 21:28:16 +0800

    拆开生成和部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-rm -rf javaclient
-java -jar swagger-codegen-cli-laiyijie.jar generate -DuseRxJava=true -i main.yaml -l java -o ./javaclient -c ./codegen_config/javaclientconfig.json
-rm -rf spring
-java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l spring -o ./spring -c ./codegen_config/springconfig.json
-rm -rf objcclient
-java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l objc -o ./objcclient -c ./codegen_config/objcclientconfig.json
-rm -rf jsclient
-java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l typescript-angular -o ./jsclient -c ./codegen_config/angularjsclientconfig.json
-cnpm install 
-tsc
 npm version 1.0.$(date +%s)
 npm login  <<EOF
 bangnongmang
