90aa278c7549e0e8624e387a8aac0beec4deef65
laiyijie <449132062@qq.com>
2017-09-28 21:04:52 +0800

    change the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+rm -rf javaclient
+java -jar swagger-codegen-cli-laiyijie.jar generate -DuseRxJava=true -i main.yaml -l java -o ./javaclient -c ./codegen_config/javaclientconfig.json
+rm -rf spring
+java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l spring -o ./spring -c ./codegen_config/springconfig.json
+rm -rf objcclient
+java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l objc -o ./objcclient -c ./codegen_config/objcclientconfig.json
 rm -rf jsclient
 java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l typescript-angular -o ./jsclient -c ./codegen_config/angularjsclientconfig.json
 cnpm install 
