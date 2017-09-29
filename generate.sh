d0fc75b28f0ffa13aa42d2ce6d403122f1cf20bc
laiyijie <449132062@qq.com>
2017-09-29 22:39:17 +0800

    that the spring server can install package

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@ set -x
 rm -rf javaclient
 java -jar swagger-codegen-cli-laiyijie.jar generate -DuseRxJava=true -i main.yaml -l java -o ./javaclient -c ./codegen_config/javaclientconfig.json
 rm -rf spring
+cp -r spring_server_config spring
 java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l spring -o ./spring -c ./codegen_config/springconfig.json
 rm -rf objcclient
 java -jar swagger-codegen-cli-laiyijie.jar generate -i main.yaml -l objc -o ./objcclient -c ./codegen_config/objcclientconfig.json
